7ae6c9d43a551e81568e34e10b84a2e5d1e3b7ce
Bleakness.Kozel <2hipcore@gmail.com>
2015-09-25 23:22:12 +0300

    New Homework
    Homework Methods

---
+++ 01.BiggerNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiggerNumber_1
+{
+    class BiggerNumber_1
+    {
+        static int GetMax(int a, int b)
+        {
+            int c = Math.Max(a, b);
+            return c;
+        }
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+
+            int max = GetMax(first, second);
+            Console.WriteLine(max);
+        }
+    }
+}
